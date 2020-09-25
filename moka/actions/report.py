e2b7b4d5cb144071f0ebf5acfe8dced5eb760571
felipez <tifonzafel@gmail.com>
2020-09-25 13:18:36 +0200

    add option to report the computed data

---
+++ moka/actions/report.py
@@ -0,0 +1,33 @@
+"""Module to move the data back to the server.
+
+API
+---
+.. autofunction:: report_properties
+
+"""
+
+from pathlib import Path
+
+import pandas as pd
+
+from ..client import query_server
+from ..utils import Options
+
+
+
+def read_properties_from_csv(path_results: Path) -> pd.DataFrame:
+    """From a csv file to a pandas DataFrame."""
+    if not path_results.exists():
+        raise RuntimeError(f"{path_results} doesn't exists!")
+    df = pd.read_csv(path_results).reset_index(drop=True)
+    return df.loc[:, ~df.columns.str.contains('^Unnamed')]
+
+
+def report_properties(opts: Options) -> None:
+    """Send computed properties to the server."""
+    pass
+    # # Read the data
+    # df = read_properties_from_csv(Path(opts.path_results))
+    # for _, row in df.iterrows():
+    #     query_server.
+
