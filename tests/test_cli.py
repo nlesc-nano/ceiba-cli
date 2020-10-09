02132c6d8b89557df9594e8f1af5df79b0dfc5d4
felipez <tifonzafel@gmail.com>
2020-10-09 10:03:43 +0200

    Mock the command line interface

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,5 +1,36 @@
 """Test the command line interface."""
 
-def test_cli():
+import argparse
+
+from pathlib import Path
+from pytest_mock import MockFixture, mocker
+
+from moka.cli import main
+
+from .utils_test import PATH_TEST
+
+
+def run_workflow_mock(mocker: MockFixture, action: str, path_input: Path) -> None:
+    """Run mocked action."""
+    functions = {"add": "add_jobs",
+                 "compute": "compute_jobs",
+                 "query": "query_properties",
+                 "report": "report_properties"}
+
+    # Mock argparse
+    mocker.patch("argparse.ArgumentParser.parse_args", return_value=argparse.Namespace(
+        command=action, input=path_input))
+
+    # Mock the action
+    mocker.patch(f"moka.cli.{functions[action]}", return_value=None)
+
+    main()
+
+
+def test_cli(mocker: MockFixture):
     """Test the command line interface."""
-    pass
+    actions = ("compute", "query", "report", "add")
+
+    for action in actions:
+        path_input = PATH_TEST / f"input_test_{action}.yml"
+        run_workflow_mock(mocker, action, path_input)
