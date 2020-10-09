9da8b625ffb981b6305c112f740d90de30f3b905
felipez <tifonzafel@gmail.com>
2020-10-09 16:52:22 +0200

    add more test for compute

--- tests/test_compute.py
+++ tests/test_compute.py
@@ -1,10 +1,11 @@
 """Test the compute method."""
 
-
+import pytest
 from pytest_mock import MockFixture, mocker
 
-from moka.actions import compute_jobs
+from moka.actions.compute import compute_jobs, update_job_status
 from moka.input_validation import validate_input
+from moka.utils import Options
 
 from .utils_test import PATH_TEST, read_mocked_reply
 
@@ -22,3 +23,27 @@ def test_compute(mocker: MockFixture):
                  return_value=None)
 
     compute_jobs(opts)
+
+
+def test_no_jobs_to_compute(mocker: MockFixture):
+    """Test that the application exits without error if there are no jobs."""
+    path_input = PATH_TEST / "input_test_compute.yml"
+    opts = validate_input(path_input, "compute")
+
+    # Mock the server call
+    mocker.patch("moka.actions.compute.query_server",
+                 return_value={"jobs": []})
+
+    with pytest.raises(SystemExit):
+        compute_jobs(opts)
+
+
+def test_update_status(mocker: MockFixture):
+    """Test the update update_job_status function."""
+    mocker.patch("moka.actions.compute.query_server",
+                 return_value=None)
+
+    opts = Options({"collection_name": "something"})
+    job = {"_id": 314159265}
+
+    update_job_status(opts, job, 'FAILED')
