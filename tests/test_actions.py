73d2d94d53449f2e4a5b0aaa3cd3cd1c58455884
felipez <tifonzafel@gmail.com>
2020-10-09 10:40:52 +0200

    add compute test

--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,11 +1,12 @@
 """Module to the client actions."""
 
 import json
-
 from pathlib import Path
+from typing import Any, Dict
+
 from pytest_mock import MockFixture, mocker
 
-from moka.actions import query_properties, report_properties
+from moka.actions import compute_jobs, query_properties, report_properties
 from moka.input_validation import validate_input
 
 from .utils_test import PATH_TEST
@@ -18,7 +19,17 @@ def test_add_jobs():
 
 def test_compute(mocker: MockFixture):
     """Test the functionality to compute jobs."""
-    pass
+    path_input = PATH_TEST / "input_test_compute.yml"
+    opts = validate_input(path_input, "compute")
+
+    # Mock the server call
+    mocker.patch("moka.actions.compute.query_server",
+                 return_value=read_mocked_reply("compute_jobs_mocked.json"))
+
+    mocker.patch("moka.actions.compute.update_job_status",
+                 return_value=None)
+
+    compute_jobs(opts)
 
 
 def test_report(mocker: MockFixture):
@@ -39,13 +50,18 @@ def test_query(mocker: MockFixture, tmp_path: Path):
     opts = validate_input(path_input, "query")
     opts.output_file = (tmp_path / "output.csv").absolute().as_posix()
 
-    # Read mock reply
-    path_reply = PATH_TEST / "reply_mock.json"
-    with open(path_reply, 'r') as handler:
-        reply = json.load(handler)
-
-    # Mock the server interface
-    mocker.patch("moka.actions.query.query_server", return_value=reply)
+    # Mock the server call
+    mocker.patch("moka.actions.query.query_server",
+                 return_value=read_mocked_reply("query_mocked.json"))
 
     df = query_properties(opts)
     assert len(df) == 10
+
+
+def read_mocked_reply(file_name: str) -> Dict[str, Any]:
+    """Read a mocked JSON reply from ``file_name``."""
+    path_reply = PATH_TEST / "mocked_replies" / file_name
+    with open(path_reply, 'r') as handler:
+        reply = json.load(handler)
+
+    return reply
