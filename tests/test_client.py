223124ef371aea436ffed408f2aebbe69506f25a
felipez <tifonzafel@gmail.com>
2020-10-20 10:33:21 +0200

    add more tests for the client

--- tests/test_client.py
+++ tests/test_client.py
@@ -2,11 +2,38 @@
 
 import json
 
-from moka.client import query_server
+import pytest
+from pytest_mock import MockerFixture
 
+from moka.client import query_server
+from moka.utils import Options
 
 def test_client():
     """Test the request funcionality."""
     data = json.loads(query_server('http://httpbin.org/post', "value"))
 
     assert data["query"] == "value"
+
+
+def test_client_405():
+    """Check that the client recieve a 405 error when making a wrong request."""
+    url = "https://data.rcsb.org/rest/v1/core/pubmed/4ACQ"
+
+    with pytest.raises(RuntimeError) as info:
+        query_server(url, "")
+
+    error = info.value.args[0]
+    assert "Error 405" in error
+
+
+def test_client_error(mocker: MockerFixture):
+    """Check that an error is raise if the reply contains some errors."""
+    reply = Options(
+        {"status_code": 200, "text": '{"errors": "Something went wrong!"}'})
+    mocker.patch("requests.post", return_value=reply)
+
+    with pytest.raises(RuntimeError) as info:
+        query_server("https://awesomeness.org", "")
+
+    error = info.value.args[0]
+    assert "Something went wrong!" in error
